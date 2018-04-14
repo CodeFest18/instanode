4b388fdfe53c551c8e39f597142be07b3fdbbd97
ryan <rmb280@scarletmail.rutgers.edu>
2018-04-14 19:30:09 -0400

    can now append json objects to a blockchain

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,47 @@
 import flask
 import time
 import requests
-import threading
+import hashlib as hasher
+import datetime as date
+
+class Block:
+  def __init__(self, index, timestamp, data, previous_hash):
+    self.index = index
+    self.timestamp = timestamp
+    self.data = data
+    self.previous_hash = previous_hash
+    self.hash = self.hash_block()
+  
+  def hash_block(self):
+    sha = hasher.sha256()
+    sha.update((str(self.index) + 
+               str(self.timestamp) + 
+               str(self.data) + 
+               str(self.previous_hash)).encode('utf-8'))
+    return sha.hexdigest()
+
+
+def create_genesis_block():
+  # Manually construct a block with
+  # index zero and arbitrary previous hash
+  return Block(0, date.datetime.now(), "Genesis Block", "0")
+
+
+def next_block(last_block, json_transaction):
+  this_index = last_block.index + 1
+  this_timestamp = date.datetime.now()
+  this_data = json_transaction
+  this_hash = last_block.hash
+  return Block(this_index, this_timestamp, this_data, this_hash)
 
 app = flask.Flask(__name__)
 
-@app.route('/')
+BLOCKCHAIN = [create_genesis_block()]
+
+@app.route('/', methods=['POST'])
 def home():
+	BLOCKCHAIN.append(next_block(BLOCKCHAIN[-1], flask.request.json))
+	print(list(map(lambda x:x.__dict__, BLOCKCHAIN)))	
 	return flask.render_template('home.html')
 
 @app.route('/gui')
