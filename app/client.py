59f25c2b42620eefa533414047e77be134a20777
ryan <rmb280@scarletmail.rutgers.edu>
2018-04-14 16:26:09 -0400

    opened flask server to all ip addrs

--- app/client.py
+++ app/client.py
@@ -4,6 +4,7 @@ import time
 while True:	
 	try:
 		for host in "18.222.52.182","13.59.211.35","18.188.5.1":
+			print("POLLING: " + host)
 			res = requests.get("http://"+host+":5000/")
 			print(res.__dict__)
 	except Exception as e:
