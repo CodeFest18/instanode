03b51b56ebcbe4c68ce4c411a03798e02729e694
Paul <pyp24@drexel.edu>
2018-04-14 22:43:16 -0400

    Replace print with printLog file
    Edited Replace print with printLog file

--- debugrn.py
+++ debugrn.py
@@ -1,4 +1,13 @@
-def printLog(*args, **kwargs): #Get print arguments
-    #Change arguments of print
-    print(*args, **kwargs)
+from datetime import datetime
+import socket
+
+def printLog(*args, **kwargs):
+    msg = ' '.join([str(item) for item in args])
+#Change arguments of print
+    #open file
+    file = open("log.txt", "a")
+    #get date as a string
+    date = datetime.now()
+    file.write(str(date) + " " + msg + "\n")
+    file.close()
     
