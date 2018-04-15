1805009e41c9f480f8bfcf417181f46501807815
ryan <rmb280@scarletmail.rutgers.edu>
2018-04-15 07:14:07 -0400

    client runs once instead of in an inf loop

--- app/client.py
+++ app/client.py
@@ -5,38 +5,35 @@ import hashlib as hasher
 
 HOSTNAME=socket.gethostname()
 
-while True:	
-	block_hashes = {}
-	largest_hash_count = 0
-	number_of_nodes = 0
-	mode_hash = 0
-	try:
-		# BEGIN CONSENSUS
-		for host in ["172.31.27.255","172.31.21.220","172.31.24.15"]:
-			print("POLLING: " + host)
-			res = requests.get("http://"+host+":5000/blockchain-hash")
-			block_hash = res._content
+block_hashes = {}
+largest_hash_count = 0
+number_of_nodes = 0
+mode_hash = 0
+try:
+  # BEGIN CONSENSUS
+  for host in ["172.31.27.255","172.31.21.220","172.31.24.15"]:
+    print("POLLING: " + host)
+    res = requests.get("http://"+host+":5000/blockchain-hash")
+    block_hash = res._content
 
-			if block_hash in block_hashes:
-				block_hashes[block_hash] +=1
+    if block_hash in block_hashes:
+      block_hashes[block_hash] +=1
 
-			else:
-				block_hashes[block_hash] = 1
-			
-			for hash_count in block_hashes:
-				if block_hashes[hash_count] > largest_hash_count:
-					largest_hash_count = block_hashes[hash_count]
+    else:
+      block_hashes[block_hash] = 1
+    
+    for hash_count in block_hashes:
+      if block_hashes[hash_count] > largest_hash_count:
+        largest_hash_count = block_hashes[hash_count]
 
-			number_of_nodes += 1
+    number_of_nodes += 1
 
-		if largest_hash_count < number_of_nodes / 2:
-			print("ERROR IN CONSENSUS")
-			print("NUMBER OF NODES:", number_of_nodes)
-			print("LARGEST HASH COUNT: ", largest_hash_count)
+  if largest_hash_count < number_of_nodes / 2:
+    print("ERROR IN CONSENSUS")
+    print("NUMBER OF NODES:", number_of_nodes)
+    print("LARGEST HASH COUNT: ", largest_hash_count)
 
-		# END CONSENSUS
+  # END CONSENSUS
 
-	except Exception as e:
-		print(e)
-
-	time.sleep(1)
+except Exception as e:
+  print(e)
