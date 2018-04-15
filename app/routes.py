07b7d9dc6d4842132f71e7d021e815d95b602560
ryan <rmb280@scarletmail.rutgers.edu>
2018-04-15 05:38:12 -0400

    debug prints

--- app/routes.py
+++ app/routes.py
@@ -53,6 +53,7 @@ PUB_KEY_STR = PUB_KEY.public_bytes(
 with subprocess.Popen(["hostname", "-i"], stdout=subprocess.PIPE) as hostname_proc:
       ip_addr = hostname_proc.stdout.read().strip()
 hosts = [host for host in ["172.31.27.255","172.31.21.220","172.31.24.15"] if host != ip_addr]
+print(hosts)
 
 @app.route('/determine-if-leader')
 def leader_determine():
