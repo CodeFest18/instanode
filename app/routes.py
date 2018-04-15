b23332783bbac7a67e2edee481679e1d68401d03
ryan <rmb280@scarletmail.rutgers.edu>
2018-04-14 22:09:35 -0400

    removed timestamp from genesis block so all nodes can generate the same block

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,8 @@ import hashlib as hasher
 import datetime as date
 
 class Block:
-  def __init__(self, index, timestamp, data, previous_hash):
+  def __init__(self, index, data, previous_hash):
     self.index = index
-    self.timestamp = timestamp
     self.data = data
     self.previous_hash = previous_hash
     self.hash = self.hash_block()
@@ -15,7 +14,6 @@ class Block:
   def hash_block(self):
     sha = hasher.sha256()
     sha.update((str(self.index) + 
-               str(self.timestamp) + 
                str(self.data) + 
                str(self.previous_hash)).encode('utf-8'))
     return sha.hexdigest()
@@ -24,7 +22,7 @@ class Block:
 def create_genesis_block():
   # Manually construct a block with
   # index zero and arbitrary previous hash
-  return Block(0, date.datetime.now(), "Genesis Block", "0")
+  return Block(0, "Genesis Block", "0")
 
 
 def next_block(last_block, json_transaction):
